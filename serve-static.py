eedb9548139e87d78852de4ce302ad0bd861cdf8
Peter Renshaw <goonmail@netspace.net.au>
2013-08-22 11:25:31 +1000

    Update serve-static.py

--- serve-static.py
+++ serve-static.py
@@ -1,6 +1,31 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+
+#===
+# name  serve-static.py
+# date: 2013AUG18
+# prog: pr
+# desc: simple static web server using tornado
+#       not sure if this will in final code but
+#       for the moment this will do. At a future
+#       time I'll be re-writing this code.
+#
+# copy: copyright (C) 2013 Nykakin
+#       <http://stackoverflow.com/questions/17284286/disable-template-processing-in-tornadoweb>
+#  
+#       This code is a nice little hack by Nykakin allowing tornado
+#       to serve AngularJS and use the Angular templating
+#       instead of the tornado templating which has the 
+#       same/similar templating notation,
+#
+# note: 'nice hack Nykakin, is this the standard way AngularJS should be used 
+#       with tornado to get around the template notation. Q (I ask myself) 
+#       Is there a way to turn templating off in tornado?' cf url.
+#===
+
+
+
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
@@ -12,7 +37,7 @@ from tornado.options import define, options
 define("port", default=8080, help="run on the given port", type=int)
 
 import os 
-angular_app_path=os.path.join(os.path.dirname(__file__), "app")
+angular_app_path=os.path.join(os.path.dirname(__file__), "app-1")
 
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
@@ -33,3 +58,4 @@ if __name__ == "__main__":
     http_server.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
 
+# vim: ff=unix:ts=4:sw=4:tw=78:noai:expandtab 
