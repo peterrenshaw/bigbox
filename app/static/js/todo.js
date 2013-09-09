f4aa523da0cf2219c99357e0e7ccbd46046ff134
Peter Renshaw <goonmail@netspace.net.au>
2013-09-09 19:40:09 +1000

    2013SEP09 * work on front-end - index.html - js/controller.js

--- app/static/js/todo.js
+++ app/static/js/todo.js
@@ -3,33 +3,33 @@
 */
 angular.module('bigbox', ['ui.bootstrap']);
 function TodoCtrl($scope) {
-    $scope.commands = ['!ddg', '!duckduckgo', '!twit', '!twitter', ':clr', ':clear',':h',
+        $scope.commands = ['!ddg', '!duckduckgo', '!twit', '!twitter', ':clr', ':clear',':h',
                        ':help', ':img', ':loc', ':local', ':per', ':person', ':q', 
                        ':query', ':sav', ':save', ':url', ':w', ':words'];
-    $scope.todos = [
-        {text:'learn angular', done:true},
-        {text:'build an angular app', done:false}];
+        $scope.todos = [
+            {text:'learn angular', done:true},
+            {text:'build an angular app', done:false}];
      
         $scope.addTodo = function() {
-        if ( $scope.todoText ) {
-            $scope.todos.push({text:$scope.todoText, done:false});
-            $scope.todoText = '';
-        }
-    };
+            if ( $scope.todoText ) {
+                $scope.todos.push({text:$scope.todoText, done:false});
+                $scope.todoText = '';
+            }
+        };
      
-    $scope.remaining = function() {
-        var count = 0;
-        angular.forEach($scope.todos, function(todo) {
-            count += todo.done ? 0 : 1;
-        });
-        return count;
-    };
+        $scope.remaining = function() {
+            var count = 0;
+            angular.forEach($scope.todos, function(todo) {
+                count += todo.done ? 0 : 1;
+            });
+            return count;
+        };
      
-    $scope.archive = function() {
-        var oldTodos = $scope.todos;
-        $scope.todos = [];
-        angular.forEach(oldTodos, function(todo) {
-            if (!todo.done) $scope.todos.push(todo);
-        });
-    };
+        $scope.archive = function() {
+            var oldTodos = $scope.todos;
+            $scope.todos = [];
+            angular.forEach(oldTodos, function(todo) {
+                if (!todo.done) $scope.todos.push(todo);
+            });
+        };
 }
