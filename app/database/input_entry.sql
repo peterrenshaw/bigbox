7b6fc986bd245955232d9389f1e47abe6347ce8c
Peter Renshaw <goonmail@netspace.net.au>
2013-09-10 10:36:41 +1000

    2013SEP10 * hack 1 - add data to bigbox.entry table - run query over data & export to text

---
+++ app/database/input_entry.sql
@@ -0,0 +1,69 @@
+-- #===
+-- # name: input_sql
+-- # date: 2013SEP10
+-- # prog: pr
+-- # desc: sql queries to input entry data
+-- #===
+
+
+-- #---
+-- # USAGE: IN:  sqlite3 bigbox.db < input_sql
+-- #        OUT: sqlite3 -header -column -echo bigbox.db < query.sql > data.txt
+-- #---
+
+-- #---
+-- # Person
+-- #---
+
+
+INSERT INTO Entry (line, date_time, length, flagged)
+VALUES ('#SMEM WATCH AND ACT http://bit.ly/19C4HDI  Watch and Act DOHERTYS RD LAVERTON NORTH Effective: 10/09/13 9:50 AM Expires: 10/09/13 4:00...
+',0,0,0);
+
+INSERT INTO Entry (line, date_time, length, flagged)
+VALUES ('#SMEM Former army barracks at Brighton destroyed by fire - Herald Sun http://bit.ly/17lVYVW',0,0,0);
+
+INSERT INTO Entry (line, date_time, length, flagged)
+VALUES ('#vicses Minor Flood Warning for the Kiewa River at Bandiana http://bit.ly/17SqDct',0,0,0);
+
+INSERT INTO Entry (line, date_time, length, flagged)
+VALUES ('#SMEM WATCH AND ACT http://bit.ly/13CubQu',0,0,0);
+
+INSERT INTO Entry (line, date_time, length, flagged)
+VALUES ('#SMEM ADVICE http://bit.ly/13Cu9rV',0,0,0);
+
+INSERT INTO Entry (line, date_time, length, flagged)
+VALUES ('#SMEM WATCH AND ACT http://bit.ly/13CubQu  Watch and Act DOHERTYS RD LAVERTON NORTH Effective: 10/09/13 7:24 AM Expires: 10/09/13 1:00...',0,0,0);
+
+INSERT INTO Entry (line, date_time, length, flagged)
+VALUES ('#SMEM ADVICE http://bit.ly/13Cu9rV  Advice - All Clear BANANA AL MELBOURNE Effective: 10/09/13 7:14 AM Expires: 11/09/13 7:00 AM Inci...',0,0,0);
+
+INSERT INTO Entry (line, date_time, length, flagged)
+VALUES ('It`s currently Light Rain Shower and 12C outside in Melbourne #vicrains',0,0,0);
+
+INSERT INTO Entry (line, date_time, length, flagged)
+VALUES ('#SMEM ADVICE http://bit.ly/19B7gps',0,0,0);
+
+INSERT INTO Entry (line, date_time, length, flagged)
+VALUES ('#SMEM ADVICE http://bit.ly/19B7gps  Advice BANANA AL MELBOURNE Effective: 10/09/13 3:27 AM Expires: 10/09/13 8:00 AM Incident Informa...',0,0,0);
+
+INSERT INTO Entry (line, date_time, length, flagged)
+VALUES ('Laverton North: MFB Watch & Act: http://www.mfb.vic.gov.au/news/warning.html?page=10%2f09%2f2013-9%3a51%3a06-AM … pic.twitter.com/daoh47hBhVLaverton North: MFB Watch & Act: http://www.mfb.vic.gov.au/news/warning.html?page=10%2f09%2f2013-9%3a51%3a06-AM … pic.twitter.com/daoh47hBhV',0,0,0);
+
+INSERT INTO Entry (line, date_time, length, flagged)
+VALUES ('Laverton North: MFB Watch & Act: http://www.mfb.vic.gov.au/news/warning.html?page=10%2f09%2f2013-9%3a51%3a06-AM … pic.twitter.com/daoh47hBhV',0,0,0);
+
+INSERT INTO Entry (line, date_time, length, flagged)
+VALUES ('Also here - Smoke over the freeway near the fire at the Nth Laverton Car Recycling Depot. Thanks to Josh P http://ow.ly/i/36iBk',0,0,0);
+
+INSERT INTO Entry (line, date_time, length, flagged)
+VALUES ('@cooper123_j Recycling yard. Stay out of toxic smoke.',0,0,0);
+
+INSERT INTO Entry (line, date_time, length, flagged)
+VALUES ('@tyabblemons "What`s in the bong, man?" "Linkedin Man"',0,0,0);
+
+INSERT INTO Entry (line, date_time, length, flagged)
+VALUES ('Laverton North: Around 22 appliances now on that recycling yard fire. Many road closures. Stay clear of toxic smoke.',0,0,0);
+
+INSERT INTO Entry (line, date_time, length, flagged)
+VALUES ('Dohertys Rd, Laverton North - Road Closed, Doherty`s Rd is closed between Fitzgerald Rd and Foundation… http://bit.ly/1fPY81V  #victraffic',0,0,0);
