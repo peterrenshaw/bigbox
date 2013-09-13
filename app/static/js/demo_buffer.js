13a09f82d85e066ab5e291caaeac7d7b8e004e7c
Peter Renshaw <goonmail@netspace.net.au>
2013-09-13 14:19:24 +1000

    2013SEP13 * demo - getting ready for demo

---
+++ app/static/js/demo_buffer.js
@@ -0,0 +1,27 @@
+var bigbox = angular.module('bigbox', ['ngResource'])
+bigbox.factory('Search', function ($resource) {
+    return $resource('http://127.0.0.1\\:8081/bb/api/v1.0/e/:term', {}, {
+        query: {
+            method: 'GET',
+            isArray: false,
+            params: {
+                term: "term"
+            }
+        }
+    });
+});
+
+function BigboxCtrl($scope, Search) {
+    $scope.show = "";
+    $scope.bufferText = "";
+    $scope.search = function(term) {
+        Search.get({term: term}, 
+        function(results) {
+            $scope.show = term;
+            $scope.results = results;
+        });
+    }
+    $scope.entryText = "";
+}
+
+
