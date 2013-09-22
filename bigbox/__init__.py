db7df3cb98a41d8435e123278092ea93e7eef3e2
Peter Renshaw <goonmail@netspace.net.au>
2013-09-22 21:32:26 +1000

    2013SEP22 * missing files

---
+++ bigbox/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# ~*~ encoding: utf-8 ~*~
+
+
+__version__ = '0.0.1'
+__author__ = 'Peter Renshaw'
+__license__ = 'GNU GPL 3.0'
+
+
+__all__ = ['bigbox','feed']
+
+
+# vim: ff=unix:ts=4:sw=4:tw=78:noai:expandtab
